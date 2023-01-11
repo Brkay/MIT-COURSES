30896a40d354626120d13721c2e07df36a2f536d
Berkay Yaldız <67972187+Brkay@users.noreply.github.com>
2023-01-11 21:45:31 +0300

    Add files via upload

---
+++ 6.002 INTRODUCTION TO COMPUTATIONAL THINKING AND DATA SCIENCE/PS4/random deneme.py
@@ -0,0 +1,11 @@
+import random
+
+random.seed(0)
+# holder = []
+# number_of_iterations = 100000  # True!
+# for i in range(number_of_iterations):
+#     if random.random() <= 0.8:
+#         holder.append(True)
+#     else:
+#         holder.append(False)
+# print(sum(holder) / number_of_iterations)
